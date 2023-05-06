3789a32eda7eb0d26af13898abd3a62874792d1d
NorichikaSugii <wawa60379@gmail.com>
2023-05-07 03:31:33 +0900

    Initial Commit

---
+++ Profile/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Profile
+//
+//  Created by 杉井位次 on 2023/05/07.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
